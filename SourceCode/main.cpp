0f59b2c5c876e62701efe2e8d5cc3d8add9407b8
erlendak <erlendak@mail.uio.no>
2019-12-19 22:01:31 +0100

    Last thouch.

--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -12,30 +12,47 @@ using namespace std;
 
 
 int main(){
+    /*
+    In this project we are trying to simulate the temperature distrubution in the lithosphere outside
+    the coast of Norway. To see if we could find evidence of a subduction zone that was refertilitating
+    the mantle wedge. To handle this problem we are analysing methods in both (1+1) and (2+1) dimentions,
+    to check up upon if these methods work as intended, which method is best for our simulation and if
+    they are suitable for the simulation we need.
+
+    To do so we are testing the Forward Euler, Backward Euler and Cran Nicolson in (1+1) dimention, and
+    an implicit method in (2+1) dimentions. Through analytical solutions as unit tests and through
+    graphical analycis.
+
+    In the end we are simulating the temperature distrubution over a giga year of time, then we are analysing
+    the results graphicaly to see if there should be some kind of evidence of such a event.
+    */
+
+    //Initialize time operator ;
+    clock_t start, finish;
+
+
+    //Unit tests.
 
     test_jacobi_solver();
     test_forward_euler();
     test_backward_euler();
     test_crank_nicolson();
 
-    //Initialize variabels used multiple times ;
-    clock_t start, finish; // initialize time operators.
 
     // Setup examples ;
-    double delta_x1 = 0.1;
-    double delta_x2 = 0.01;
-    double delta_t = 0.00005;
-    double example_lenght = 1;
-    double example_time = 1;
-    int example_n1 =  (int)(example_lenght/delta_x1);
-    int example_n2 = (int)(example_lenght/delta_x2);
 
-    // Setup inital conditions for example.
+    double delta_x1 = 0.1;     // Set delta x for example 1.
+    double delta_x2 = 0.01;    // Set delta x for example 2.
+    double delta_t = 0.00005;  // Set delta t for both example 1. and 2.
+    double example_lenght = 1; // Set lenght for both example.
+    double example_time = 1;   // Set the langht of time in both examples.
+    int example_n1 =  (int)(example_lenght/delta_x1); // Amounted of points needed in solution of example 1.
+    int example_n2 = (int)(example_lenght/delta_x2);  // Amounted of points needed in solution of example 2.
+
+    // Setup inital conditions for both example 1. and 2.
     vec u1 = zeros<vec>(example_n1+1);     // For example with delta x = 0.1.
     vec u2 = zeros<vec>(example_n2+1);     // For example with delta x = 0.01.
     u1(example_n1) = u2(example_n2) = 1;   // Boundary condition.
-
-
 
 
     //Forward Euler
@@ -71,8 +88,6 @@ int main(){
     " Meters.\n(Simulation time ; " << ( ( (double)finish - (double)start ) /CLOCKS_PER_SEC)<<" Seconds)\n" <<endl;
 
 
-
-
     //Backward Euler ;
 
 
@@ -90,6 +105,7 @@ int main(){
     cout<<"Example simulation using Backward Euler with a time step of ; "<< delta_t << " seconds and a delta x ; "<< delta_x1<<
     " Meters.\n(Simulation time ; " << ( ( (double)finish - (double)start ) /CLOCKS_PER_SEC)<<" Seconds)\n" <<endl;
 
+
     //Delta X ; 0.01
 
     ofstream dfile;
@@ -103,8 +119,6 @@ int main(){
 
     cout<<"Example simulation using Backward Euler with a time step of ; "<< delta_t << " seconds and a delta x ; "<< delta_x2<<
     " Meters.\n(Simulation time ; " << ( ( (double)finish - (double)start ) /CLOCKS_PER_SEC)<<" Seconds)\n" <<endl;
-
-
 
 
     // Crank Nicolson ;
@@ -124,6 +138,7 @@ int main(){
     cout<<"Example simulation using Crank Nicolson with a time step of ; "<< delta_t << " seconds and a delta x ; "<< delta_x1<<
     " Meters.\n(Simulation time ; " << ( ( (double)finish - (double)start ) /CLOCKS_PER_SEC)<<" Seconds)\n" <<endl;
 
+
     //Delta X ; 0.01
 
     ofstream ffile;
@@ -139,14 +154,19 @@ int main(){
     " Meters.\n(Simulation time ; " << ( ( (double)finish - (double)start ) /CLOCKS_PER_SEC)<<" Seconds)\n" <<endl;
 
 
+    // Example in 2 dimention.
+
     diffusjon_example_2dim(10);
 
+    // Setup of our main simulation.
 
-    int n= 120;
-    int tsteps = 100000000;
-    double delta_x = 1000;
-    delta_t = 1000000000;
-    //double alpha = delta_t/(delta_x*delta_x);
+    int n= 120; // Amount of points in depth ; 120 Kilometers.
+    double delta_x = 1000; // Distance between each point is 1 kilometer.
+
+    // Then we find the boundary conditions for the simulation.
+    int tsteps = 100000000; // We set a maximum number of time steps.
+    delta_t = 1000000000;   // Then we set the time between each step.
+
     ofstream hfile;
     string hfilename = "RESULTS/simulation_no_enrichment.dat";
     hfile.open(hfilename);
@@ -155,26 +175,28 @@ int main(){
     hfile << setw(20) << setprecision(8) << no_enrichment <<endl;
     hfile.close();
 
-    //Grensebetingelser
-   int nx = 120;
-   int ny = 150;
-   double dxy=1000;
-   double dt = delta_t;
+   //Then we find the initial conditions for the simulation.
+   int nx = n;    // Set amount of points in depth ; 120 Kilometers.
+   int ny = 150;  // Amount of in the limited radioactive area ; 150 Kilometers.
+   double dxy = 1000;   // Set to a 1 kilometer distance between each point.
+   double dt = delta_t; // Then we set the time between each step.
    mat A = zeros<mat>(nx,ny);
 
-   // Inital conditions
+   // Inital conditions.
    for(int j =0; j<ny; j++){
        for(int i =0; i<nx; i++){
-           A(i,j)    = no_enrichment(i); //Depth conditions use vector from task above
-           //A(i,ny-1) = 1; //Depth conditions use vector from task above
+           A(i,j)    = no_enrichment(i); //Depth conditions use vector from task above.
         }
     }
+   /*
+   // Boundary conditions.
    for(int j =0; j<ny; j++){
-       A(0,j)    = 8; //Depth conditions use vector from task above
-       A(nx-1,j) = 1300; //Depth conditions use vector from task above
+       A(0,j)    = 8;    // Make sure boundary condtions is correct.
+       A(nx-1,j) = 1300; // Make sure boundary condtions is correct.
     }
-   tsteps = 1000000;
 
+   tsteps = 1000000;
+*/
 
     simulation_with_enrichment(nx, ny, dt, dxy,A, tsteps);
 
@@ -188,21 +210,25 @@ int main(){
     ifile.open(ifilename);
     ifile << setiosflags(ios::showpoint | ios::uppercase);
 
-    int n_results = 10;
+    int n_results = 10; // Amount of results we want to take from simualtion.
     tsteps = pow(10,9)*365*24*60*60/dt/n_results; // Gives us amount of time steps we need to use per result we want to note of a total of a Giga year.
-    int start_step;
-    //clock_t start, finish;
+    int start_step; // Operator that makes it posible to run the simulation in multiple intervals, and then only take n_results amount of results.
+
     for (int t = 0; t <n_results; t++){
-        start = clock();
-        start_step = t*tsteps;
-        simulation_implemented_enrichment_decay(nx, ny, dt, dxy,A, (t+1)*tsteps,start_step);
+        start = clock(); // Take time for each interval
+        start_step = t*tsteps; // Calculate the time we should start on in this interval
+        simulation_implemented_enrichment_decay(nx, ny, dt, dxy,A, (t+1)*tsteps,start_step); // Simulate interval
+
+        // Save solution of the specific interval.
         for( int i = 0; i<nx; i++){
             for(int j=0; j<ny; j++){
                 ifile << setw(20) << setprecision(8) << A(i,j);
             }
         }
         ifile<<endl;
-        finish = clock();
+        finish = clock();// Take time for each interval
+
+        // Give information on how far the simulation has come.
         cout<<"Simulated for ; "<<(t+1)*tsteps*dt/60/60/24/365/pow(10,9)<<" Giga years (Simulation time ; "<< ( ( (double)finish - (double)start ) /CLOCKS_PER_SEC)/60<<" Minutes)"  <<endl;
     }
 
